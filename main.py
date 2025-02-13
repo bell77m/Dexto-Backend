f00d29810b76dc108e571e82d9ea7ae41751ff44
bell77m <bell77m@gmail.com>
2025-02-13 14:53:37 +0700

    First Update

--- main.py
+++ main.py
@@ -24,7 +24,6 @@ app.include_router(graphql_app, prefix="/graphql")
 if __name__ == "__main__":
 
     uvicorn.run(app, host="127.0.0.1", port=8000)
-
     # cursor = mydb.cursor()
     # cursor.execute("SHOW DATABASES")
     # for db in cursor.fetchall():
