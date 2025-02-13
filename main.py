4cadfedcba6b98d9e9c966af13d967707e9323dc
bell77m <bell77m@gmail.com>
2025-02-13 17:03:02 +0700

    First Update

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from config.config import Config
 from graphql_app.schema import schema
 from fastapi import FastAPI
 from strawberry.fastapi import GraphQLRouter
@@ -22,9 +23,9 @@ graphql_app = GraphQLRouter(schema)
 app.include_router(graphql_app, prefix="/graphql")
 
 if __name__ == "__main__":
-
+    conf = Config("config/config.ini")
     #run sever
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host=conf.load_server_config()["host"], port=int(conf.load_server_config()["port"]))
 
     # cursor = mydb.cursor()
     # cursor.execute("SHOW DATABASES")
