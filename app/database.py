54a5df64653d3c00461ba22a9a1974d38a19a519
bell77m <bell77m@gmail.com>
2025-03-04 22:48:09 +0700

    First Update

--- app/database.py
+++ app/database.py
@@ -1,23 +1,34 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm import Session
 import os
+import logging
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker, Session
 from dotenv import load_dotenv
+from typing import Generator
+
 
 load_dotenv("../.env")
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 if not DATABASE_URL:
+    logger.error("DATABASE_URL is not set. Check your .env file.")
     raise ValueError("DATABASE_URL is not set. Check your .env file.")
 
+
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
 Base = declarative_base()
 
 
-def get_db() -> Session:
+def get_db() -> Generator[Session, None, None]:
     db = SessionLocal()
     try:
         yield db
